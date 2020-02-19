a60466d8132e3344a512625f2306be152e8aaf0d
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2020-02-18 16:19:23 -0800

    add support for repeat, no-repeat behavior

--- src/test/java/info/freelibrary/iiif/presentation/properties/BehaviorTest.java
+++ src/test/java/info/freelibrary/iiif/presentation/properties/BehaviorTest.java
@@ -15,11 +15,11 @@ import info.freelibrary.iiif.presentation.properties.Behavior.Option;
  */
 public class BehaviorTest {
 
-    private static final Option[] HINTS = new Option[] { Option.INDIVIDUALS, Option.PAGED, Option.CONTINUOUS,
-        Option.MULTIPART, Option.NONPAGED, Option.TOP, Option.FACINGPAGES };
+    private static final Option[] HINTS = new Option[] { Option.REPEAT, Option.NOREPEAT, Option.INDIVIDUALS,
+        Option.PAGED, Option.CONTINUOUS, Option.MULTIPART, Option.NONPAGED, Option.TOP, Option.FACINGPAGES };
 
-    private static final String[] VALUES = new String[] { "individuals", "paged", "continuous", "multi-part",
-        "non-paged", "top", "facing-pages" };
+    private static final String[] VALUES = new String[] { "repeat", "no-repeat", "individuals", "paged", "continuous",
+        "multi-part", "non-paged", "top", "facing-pages" };
 
     private static final URI[] URIS = new URI[] { URI.create("http://library.unc.edu"), URI.create(
             "http://library.ucla.edu") };
