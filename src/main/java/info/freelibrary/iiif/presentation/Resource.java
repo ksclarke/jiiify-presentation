4bd6cea8d01689349a79248898011f329c98c4c3
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2020-04-17 05:36:52 -0700

    create homepage property

--- src/main/java/info/freelibrary/iiif/presentation/Resource.java
+++ src/main/java/info/freelibrary/iiif/presentation/Resource.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import info.freelibrary.iiif.presentation.properties.Behavior;
+import info.freelibrary.iiif.presentation.properties.Homepage;
 import info.freelibrary.iiif.presentation.properties.Label;
 import info.freelibrary.iiif.presentation.properties.Logo;
 import info.freelibrary.iiif.presentation.properties.Metadata;
@@ -39,8 +40,8 @@ import io.vertx.core.json.jackson.DatabindCodec;
  */
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 @JsonPropertyOrder({ Constants.CONTEXT, Constants.TYPE, Constants.ID, Constants.LABEL, Constants.SUMMARY,
-    Constants.REQUIRED_STATEMENT, Constants.RIGHTS, Constants.PART_OF, Constants.LOGO, Constants.THUMBNAIL,
-    Constants.METADATA, Constants.SEQUENCES, Constants.SERVICE })
+    Constants.REQUIRED_STATEMENT, Constants.RIGHTS, Constants.PART_OF, Constants.HOMEPAGE, Constants.LOGO,
+    Constants.THUMBNAIL, Constants.METADATA, Constants.SEQUENCES, Constants.SERVICE })
 class Resource<T extends Resource<T>> {
 
     // We initialize this here so it loads for manifests and collections
@@ -69,6 +70,8 @@ class Resource<T extends Resource<T>> {
     private RequiredStatement myRequiredStatement;
 
     private Rights myRights;
+
+    private List<Homepage> myHomepages;
 
     private Logo myLogo;
 
@@ -438,6 +441,52 @@ class Resource<T extends Resource<T>> {
     }
 
     /**
+     * Sets the homepages for this resource.
+     *
+     * @param aHomepageArray The homepages to set for this resource
+     * @return The resource
+     */
+    @JsonSetter(Constants.HOMEPAGE)
+    public T setHomepages(final Homepage... aHomepageArray) {
+        final List<Homepage> homepages = getHomepagesList();
+
+        homepages.clear();
+        homepages.addAll(Arrays.asList(aHomepageArray));
+        return (T) this;
+    }
+
+    /**
+     * Gets the resource's homepages in an unmodifiable list.
+     *
+     * @return The resource's homepages
+     */
+    @JsonGetter(Constants.HOMEPAGE)
+    public List<Homepage> getHomepages() {
+        return Collections.unmodifiableList(getHomepagesList());
+    }
+
+    /**
+     * Adds homepages to the resource.
+     *
+     * @param aHomepageArray The homepages to add to the resource
+     * @return The resource
+     */
+    public T addHomepages(final Homepage... aHomepageArray) {
+        getHomepagesList().addAll(Arrays.asList(aHomepageArray));
+        return (T) this;
+    }
+
+    /**
+     * Removes the homepages associated with this resource.
+     *
+     * @return The resource
+     */
+    public T clearHomepages() {
+        getHomepagesList().clear();
+        return (T) this;
+    }
+
+    /**
      * Gets the logo.
      *
      * @return The logo
@@ -659,6 +708,19 @@ class Resource<T extends Resource<T>> {
         }
 
         return aBehaviorArray;
+    }
+
+    /**
+     * Gets a list of resource homepages, initializing the list if this hasn't been done already.
+     *
+     * @return A list of resource homepages
+     */
+    private List<Homepage> getHomepagesList() {
+        if (myHomepages == null) {
+            myHomepages = new ArrayList<>();
+        }
+
+        return myHomepages;
     }
 
     /**
