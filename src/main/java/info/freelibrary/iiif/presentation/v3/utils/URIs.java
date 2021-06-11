4b3ef0170f9b61ed126cee0124ef6506892b8612
Mark A. Matney, Jr <mmatney@library.ucla.edu>
2021-06-11 07:55:17 -0700

    Fix copy-paste error

--- src/main/java/info/freelibrary/iiif/presentation/v3/utils/URIs.java
+++ src/main/java/info/freelibrary/iiif/presentation/v3/utils/URIs.java
@@ -4,22 +4,22 @@ package info.freelibrary.iiif.presentation.v3.utils;
 import java.net.URI;
 
 /**
- * Constants used in this software library.
+ * URIs used in this software library.
  */
 public final class URIs {
 
     /**
-     * The constants for the Media Fragment specification URI.
+     * The URI of the <a href="http://www.w3.org/TR/media-frags/">Media Fragments URI specification</a>.
      */
     public static final URI MEDIA_FRAGMENT_SPECIFICATION_URI = URI.create("http://www.w3.org/TR/media-frags/");
 
     /**
-     * The presentation API context.
+     * The URI of the <a href="http://iiif.io/api/presentation/3/">IIIF Presentation API</a> context.
      */
     public static final URI CONTEXT_URI = URI.create("http://iiif.io/api/presentation/3/context.json");
 
     /**
-     * Creates a new constants class.
+     * Creates a new URIs class.
      */
     private URIs() {
         super();
