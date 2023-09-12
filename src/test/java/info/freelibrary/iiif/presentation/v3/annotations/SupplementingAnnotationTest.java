c070bdc7e576afecc3d14d59b55404835846f1a3
Kevin S. Clarke <ksclarke@ksclarke.io>
2023-09-12 00:00:46 -0400

    Add TextGranularity property

--- src/test/java/info/freelibrary/iiif/presentation/v3/annotations/SupplementingAnnotationTest.java
+++ src/test/java/info/freelibrary/iiif/presentation/v3/annotations/SupplementingAnnotationTest.java
@@ -20,11 +20,15 @@ import info.freelibrary.iiif.presentation.v3.Canvas;
 import info.freelibrary.iiif.presentation.v3.SpecificResource;
 import info.freelibrary.iiif.presentation.v3.SupplementingAnnotation;
 import info.freelibrary.iiif.presentation.v3.TextContent;
+import info.freelibrary.iiif.presentation.v3.TextualBody;
 import info.freelibrary.iiif.presentation.v3.properties.Label;
+import info.freelibrary.iiif.presentation.v3.properties.TextGranularity;
+import info.freelibrary.iiif.presentation.v3.properties.TextGranularity.Level;
 import info.freelibrary.iiif.presentation.v3.properties.TimeMode;
 import info.freelibrary.iiif.presentation.v3.properties.behaviors.InvalidBehaviorException;
 import info.freelibrary.iiif.presentation.v3.properties.behaviors.ManifestBehavior;
 import info.freelibrary.iiif.presentation.v3.properties.behaviors.ResourceBehavior;
+import info.freelibrary.iiif.presentation.v3.properties.selectors.FragmentSelector;
 import info.freelibrary.iiif.presentation.v3.properties.selectors.MediaFragmentSelector;
 import info.freelibrary.iiif.presentation.v3.utils.TestUtils;
 
@@ -35,6 +39,10 @@ public class SupplementingAnnotationTest extends AbstractTest {
 
     /** A test annotation. */
     private static final File ANNOTATION = new File(TestUtils.TEST_DIR, "annotation-supplementing-full.json");
+
+    /** A test resource that includes a textGranularity property. */
+    private static final File TEXT_GRANULARITY =
+            new File(TestUtils.TEST_DIR, "annotation-supplementing-text-granularity.json");
 
     /** A test canvas label. */
     private final Label myCanvasLabel = new Label(myLoremIpsum.getWords(4));
@@ -181,5 +189,28 @@ public class SupplementingAnnotationTest extends AbstractTest {
                 new SupplementingAnnotation(myAnnoID, myCanvas).setBody(content).setTarget(new Target(myCanvasID));
 
         assertEquals(format(StringUtils.read(ANNOTATION)), format(toJson(annotation)));
+    }
+
+    /**
+     * Tests serializing a supplementing annotation that contains a text granularity.
+     *
+     * @throws IOException If there is trouble reading the annotation file or serializing the constructed annotation
+     */
+    @Test
+    public final void testSerializationWithTextGranularity() throws IOException {
+        final String annoID = "https://example.org/iiif/aeneid/book1/transcription-line1";
+        final Canvas canvas = new Canvas("https://example.org/aeneid/canvas/1r");
+        final SupplementingAnnotation annotation = new SupplementingAnnotation(annoID, canvas);
+        final FragmentSelector selector = new MediaFragmentSelector("xywh=500,1100,3500,100");
+        final SpecificResource specificResource = new SpecificResource(canvas.getID(), selector);
+        final TextualBody body = new TextualBody();
+
+        body.setLanguage("la");
+        body.setValue("arma virumque cano, Troiae qui primus ab oris");
+
+        annotation.setTextGranularity(new TextGranularity(Level.LINE));
+        annotation.setBody(body).setTarget(new Target(specificResource));
+
+        assertEquals(format(StringUtils.read(TEXT_GRANULARITY)), format(toJson(annotation)));
     }
 }
