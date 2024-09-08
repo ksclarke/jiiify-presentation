e1b6217d8413dd31f61767957176a5a6b7cc0277
Kevin S. Clarke <ksclarke@ksclarke.io>
2024-09-07 22:20:41 -0400

    Catchup with cookbook recipes

--- src/test/java/info/freelibrary/iiif/presentation/v3/utils/TestUtils.java
+++ src/test/java/info/freelibrary/iiif/presentation/v3/utils/TestUtils.java
@@ -1,6 +1,7 @@
 
 package info.freelibrary.iiif.presentation.v3.utils;
 
+import static info.freelibrary.iiif.presentation.v3.utils.JsonKeys.BODY;
 import static info.freelibrary.iiif.presentation.v3.utils.JsonKeys.CONTEXT;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -45,7 +46,7 @@ public final class TestUtils {
     public static final String TEST_DIR = "src/test/resources/json";
 
     /** The JSON properties that are okay to collapse. */
-    private static final List<String> COLLAPSIBLES = Arrays.asList(CONTEXT);
+    private static final List<String> COLLAPSIBLES = Arrays.asList(CONTEXT, BODY);
 
     /** The logger for the test utilities. */
     private static final Logger LOGGER = LoggerFactory.getLogger(TestUtils.class, MessageCodes.BUNDLE);
